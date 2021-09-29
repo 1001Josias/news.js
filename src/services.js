315ff62a70ee0aaac20bbd971efa58a48b53468b
Josias Junior <1001josias@gmail.com>
2021-09-29 19:47:40 -0300

    refactoring the code to make it more scalable.

--- src/services.js
+++ src/services.js
@@ -1,36 +1,44 @@
 const axios = require('axios').default
 const cheerio = require('cheerio').default
 
-async function fetchUrl(url) {
-   try {
-      log('{fetchUrl}: Getting the website HTML...')
-      const response = await axios.get(url)
-      return await response.data         
-   } catch (error) {
-      log(`{fetchUrl Error}:\n ${error}`)
+async function services(homepageUrl, newsClassName) {
+
+   const homepageHtml = await fetchUrl(homepageUrl)
+   const newsUrls = getNewsUrls(homepageUrl, homepageHtml, newsClassName)
+
+   return {
+      getNews,
+      newsUrls,
+      homepageUrl,
+   }
+
+   async function fetchUrl(url) {
+      try {
+         log('{fetchUrl}: Getting the website HTML...')
+         const response = await axios.get(url)
+         return await response.data         
+      } catch (error) {
+         log(`{fetchUrl Error}:\n ${error}`)
+      }
+   }
+
+   function getNewsUrls(homepageUrl, homepageHtml, newsClassName) {
+      const $ = cheerio.load(homepageHtml)
+      const elementsHtml = $.html(`.${newsClassName} a`)
+      const newsUrls = []
+      $(elementsHtml).map((index, element) => {
+         let href = element.attribs.href
+         newsUrls.push(homepageUrl + href)
+      })
+      return newsUrls
+   }
+
+   async function getNews(newsIndex) {
+      const htmlNewsBody = await fetchUrl(newsUrls[newsIndex])
+      const $ = cheerio.load(htmlNewsBody)
+      const arr = Object.values($('p'))
+      return cheerio.text(arr)
    }
 }
 
-function getNewsUrls(homepageUrl, homepageHtml, newsClassName) {
-   const $ = cheerio.load(homepageHtml)
-   const elementsHtml = $.html(`.${newsClassName} a`)
-   const newsUrls = []
-   $(elementsHtml).map((index, element) => {
-      let href = element.attribs.href
-      newsUrls.push(homepageUrl + href)
-   })
-   return newsUrls
-}
-
-async function getNews(newsIndex) {
-   const htmlNewsBody = await fetchUrl(newsUrls[newsIndex])
-   const $ = cheerio.load(htmlNewsBody)
-   const arr = Object.values($('p'))
-   return cheerio.text(arr)
-}
-
-module.exports = {
-   getNews,
-   fetchUrl,
-   getNewsUrls,
-}
+module.exports = services
