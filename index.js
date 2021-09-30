c8c716c0cddaacd68d7668093a09de8a6e1b13a2
Josias Junior <1001josias@gmail.com>
2021-09-29 21:12:33 -0300

    Add the  OGlobo portal news

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const services = require('./src/services')
 
 const allAgencyNews = {
    cointelegraph: ()=> services('https://cointelegraph.com.br', 'post-card__header'),   
+   OGlobo: ()=> services('https://oglobo.globo.com','teaser__title')
 }
 
 module.exports = allAgencyNews
