122e6f7064ad5d9c0c5d26fead8234615fdfb605
Josias Junior <1001josias@gmail.com>
2021-09-30 16:12:10 -0300

    Add the globo Esporte portal news

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const services = require('./src/services')
 
 const allAgencyNews = {
    cointelegraph: ()=> services('https://cointelegraph.com.br', 'post-card__header'),   
-   OGlobo: ()=> services('https://oglobo.globo.com','teaser__title')
+   oGlobo: ()=> services('https://oglobo.globo.com','teaser__title'),
+   globoEsporte: ()=> services('https://ge.globo.com', 'feed-media-wrapper' )
 }
 
 module.exports = allAgencyNews
