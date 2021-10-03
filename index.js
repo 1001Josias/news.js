c72a402ef9f5ef3b2183e4372ae4703b55a7c8e0
Josias Junior <1001josias@gmail.com>
2021-10-02 21:04:17 -0300

    Add the livecoins portal news

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const services = require('./src/services')
 const allAgencyNews = {
    cointelegraph: ()=> services('https://cointelegraph.com.br', 'post-card__header'),   
    oGlobo: ()=> services('https://oglobo.globo.com','teaser__title'),
-   globoEsporte: ()=> services('https://ge.globo.com', 'feed-media-wrapper' )
+   globoEsporte: ()=> services('https://ge.globo.com', 'feed-media-wrapper' ),
+   livecoins: ()=> services('https://livecoins.com.br', 'td-module-thumb'),
 }
 
 module.exports = allAgencyNews
