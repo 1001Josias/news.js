1197b7ba25b5d8129767449ba88c7bb56a0a6f00
Josias Junior <1001josias@gmail.com>
2021-10-02 21:53:58 -0300

    Add the senado federal portal news

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const allAgencyNews = {
    oGlobo: ()=> services('https://oglobo.globo.com','teaser__title'),
    globoEsporte: ()=> services('https://ge.globo.com', 'feed-media-wrapper' ),
    livecoins: ()=> services('https://livecoins.com.br', 'td-module-thumb'),
+   senadoFederal: ()=> services('https://www12.senado.leg.br/noticias', 'embed-responsive'),
 }
 
 module.exports = allAgencyNews
